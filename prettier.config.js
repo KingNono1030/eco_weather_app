199ee442c85bc6597bea4b20ec8b30d217d0988e
KingNono1030 <hoejy0821@gmail.com>
2024-12-26 10:38:13 +0900

    chore: initialize project

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+}
