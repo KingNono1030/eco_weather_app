7ebf36bee86af0e082e47a219e181ccda287d8d2
KingNono1030 <hoejy0821@gmail.com>
2024-12-26 13:26:59 +0900

    rename: rewrite base url

--- src/constants/api/weatherApi.ts
+++ src/constants/api/weatherApi.ts
@@ -1,5 +1,4 @@
-export const WEATHER_MAP_BASE_URL =
-  'https://api.openweathermap.org/data/2.5/weather'
+export const WEATHER_MAP_BASE_URL = 'https://api.openweathermap.org/data/2.5/'
 
 export const WEATHER_MAP_API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY || ''
 export const WEATHER_MAP_DEFAULT_LANGUAGE = 'kr'
